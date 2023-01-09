3a3bdd83d0a8c07436e5ec8f9414c37eb8e1c823
Eren <erndmrhn@gmail.com>
2023-01-09 19:15:01 +0300

    user ınteractions

--- frontend/src/UserSignUpPage.js
+++ frontend/src/UserSignUpPage.js
@@ -1,40 +1,54 @@
 import React from "react";
 
 class UserSignupPage extends React.Component{
+ 
+state = {
+    userName: null,
+    displayName:null,
+    password:null,
+    passwordRepeat:null
+};    
+    onChange = event => {
 
+        const { name, value} = event.target;
+
+        
+        this.setState({
+            [name]:value
+        })
+    }
+
+    
     render(){
-
         return (
          <form>
               <h1> Sign Up</h1>
-
-              <div>
-              <label>User Name</label>
-              <input/>
-              </div>
-              
-             <div>
-              <label>Display  Name</label>
-              <input/>
-             </div>
-
-             <div>
-              <label>Password</label>
-              <input type ="password"/>
-             </div>
-             <div>
-              <label>Repeat Password</label>
-              <input type ="password"/>
-             </div>
-
-             <button> Sign Up</button>
-              
-         </form>
-            
-
+                
+                <div>
+                <label>User Name</label>
+                  <input name="userName" onChange={this.onChange}/>
+                </div>             
+               
+                <div>
+                <label>Display  Name</label>
+                  <input name="displayName" onChange= { this.onChange}/>
+                </div>
+                
+                <div>
+                <label>Password</label>
+                  <input name="password" type ="password" onChange = {this.onChange}/>
+                </div>
+                
+                <div>
+                <label>Password Repeat</label>
+                  <input name="passwordRepeat" type ="password" onChange = {this.onChange}/>
+                </div>          
+               
+                <button> Sign Up</button>          
+       
+         </form>        
         );
     }
-
 }
 
 export default UserSignupPage
