663fd39233496a14f9341895e45e3ccc1b8c3ff7
Eren <erndmrhn@gmail.com>
2023-01-20 17:45:21 +0300

    UserController added @CrossOrigin

--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -3,10 +3,12 @@ package com.hoaxify.ws.user;
 import org.slf4j.Logger;
 
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class UserController {
 
