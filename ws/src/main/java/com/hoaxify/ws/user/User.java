f72d9df097271a43d357ade2e6e7ad262a895c3e
Eren <erndmrhn@gmail.com>
2023-01-23 15:07:33 +0300

    h2 database connected succesfully

--- ws/src/main/java/com/hoaxify/ws/user/User.java
+++ ws/src/main/java/com/hoaxify/ws/user/User.java
@@ -1,16 +1,24 @@
 package com.hoaxify.ws.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
-@Data  //lombok annotasyonu
+@Data
+@Entity
+@Table(name="users")
 public class User {
 
-	private String username ;
+ @Id
+ @GeneratedValue
+ private Long id;	
 	
-	private String displayName;
-	
-	private String password;
-
-		
+ private String username;
+ 
+ private String displayName;
+ 
+ private String password;
 	
 }
--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -1,25 +1,19 @@
 package com.hoaxify.ws.user;
 
 import org.slf4j.Logger;
-
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
 @RestController
 public class UserController {
 
 	private static final Logger Log = LoggerFactory.getLogger(UserController.class);
 	
 	@PostMapping("/api/1.0/users")
-	public void createUser(@RequestBody User user) {
-		
+	public void createUser(@RequestBody User user ) {
 		Log.info(user.toString());
 		
-		
 	}
-	
 }
