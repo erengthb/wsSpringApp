1163b7d9e139c55c7faddee432fa93a2a2d787ee
Eren <erndmrhn@gmail.com>
2023-01-23 19:13:46 +0300

    Including Http status message structure created

--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -3,21 +3,30 @@ package com.hoaxify.ws.user;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hoaxify.ws.shared.GenericResponse;
 
 @RestController
 public class UserController {
 
+	@Autowired
+	UserService userService ; 
+	
 	private static final Logger Log = LoggerFactory.getLogger(UserController.class);
 	
-	@Autowired
-	UserRepository userRepository;
+	@PostMapping("/api/1.0/users")	
+	public  GenericResponse createUser(@RequestBody User user ) {
+		userService.save(user);
 	
-	@PostMapping("/api/1.0/users")
-	public void createUser(@RequestBody User user ) {
-		userRepository.save(user);
+		return new GenericResponse("User succesfully created");
+		
+		
 		
 	}
+
 }
