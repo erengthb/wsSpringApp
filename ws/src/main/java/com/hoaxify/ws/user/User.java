f06d572a3ac69d9dd3e24efa2d1bfe02c5e33181
Eren <erndmrhn@gmail.com>
2023-01-23 17:47:04 +0300

    initial commit

--- ws/src/main/java/com/hoaxify/ws/user/User.java
+++ ws/src/main/java/com/hoaxify/ws/user/User.java
@@ -12,7 +12,7 @@ import lombok.Data;
 public class User {
 
  @Id
- @GeneratedValue
+ @GeneratedValue()
  private Long id;	
 	
  private String username;
--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -2,6 +2,7 @@ package com.hoaxify.ws.user;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +12,12 @@ public class UserController {
 
 	private static final Logger Log = LoggerFactory.getLogger(UserController.class);
 	
+	@Autowired
+	UserRepository userRepository;
+	
 	@PostMapping("/api/1.0/users")
 	public void createUser(@RequestBody User user ) {
-		Log.info(user.toString());
+		userRepository.save(user);
 		
 	}
 }
