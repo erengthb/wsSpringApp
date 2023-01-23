5523ba2b877fc6cff9e1b26ea4f68aa1d1bdafd9
Eren <erndmrhn@gmail.com>
2023-01-23 19:24:30 +0300

    User password encrypted

--- ws/src/main/java/com/hoaxify/ws/user/UserService.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserService.java
@@ -1,6 +1,8 @@
 package com.hoaxify.ws.user;
 
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,16 +10,19 @@ public class UserService {
 	
 	
 	UserRepository userRepository;
-		
+	
+	PasswordEncoder passwordEncoder;
  
 	public UserService(UserRepository userRepository) {
 		super();
 		this.userRepository = userRepository;
+		this.passwordEncoder= new BCryptPasswordEncoder();
 	}
 
 
 
-	public void save( User user ) {
+	public void save( User user ) {	
+		user.setPassword(this.passwordEncoder.encode(user.getPassword()));
 		userRepository.save(user);
 		
 	}
