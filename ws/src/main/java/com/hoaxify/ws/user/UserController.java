3eb510e03ab98ecc4dfea7e005c51b0f38cbbcab
Eren <erndmrhn@gmail.com>
2023-01-27 16:06:13 +0300

    User controller deleted @CrossOrigin annotation

--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -3,10 +3,10 @@ package com.hoaxify.ws.user;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hoaxify.ws.shared.GenericResponse;
@@ -18,6 +18,7 @@ public class UserController {
 	UserService userService ; 
 	
 	private static final Logger Log = LoggerFactory.getLogger(UserController.class);
+	
 	
 	@PostMapping("/api/1.0/users")	
 	public  GenericResponse createUser(@RequestBody User user ) {
