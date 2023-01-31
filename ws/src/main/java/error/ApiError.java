5f1eb03b731632508903cbcca5dc14fc4cf2573a
Eren <erndmrhn@gmail.com>
2023-01-31 11:17:12 +0300

    Validating username

---
+++ ws/src/main/java/error/ApiError.java
@@ -0,0 +1,30 @@
+package error;
+
+import java.util.Date;
+import java.util.Map;
+
+import lombok.Data;
+
+@Data
+public class ApiError {
+
+	private int status ;
+	
+	private String message;
+	
+	private String path;
+	
+	private long timestamp =  new Date().getTime();
+	
+	private Map<String , String> validationErrors;
+	
+	public ApiError(int stats , String essage , String path) {
+		this.status=status;
+		this.message = message ;
+		this.path= path ;
+		
+	}
+
+
+}
+
