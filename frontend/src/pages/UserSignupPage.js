8d64f41f8add0ae813d8b3f45a7b81aa75f5d33c
Eren <erndmrhn@gmail.com>
2023-02-03 14:59:55 +0300

    validating displayName

--- frontend/src/pages/UserSignupPage.js
+++ frontend/src/pages/UserSignupPage.js
@@ -7,13 +7,17 @@ class UserSignupPage extends React.Component {
     displayName: null,
     password: null,
     passwordRepeat: null,
-    pendingApiCall:false
+    pendingApiCall:false,
+    errors: {   }
   };
 
   onChange = event => {
     const { name, value } = event.target;
+    const errors = {...this.state.errors};
+    errors[name] = undefined 
     this.setState({
-      [name]: value
+      [name]: value,
+      errors
     });
   };
 
@@ -29,26 +33,32 @@ class UserSignupPage extends React.Component {
     };
     this.setState({pendingApiCall:true})
     
-   try{
+   try {
     const response = await signup(body);
-   } catch(error){
-   }
+   }  catch(error){
+    if(error.response.data.validationErrors){
+      this.setState({errors:error.response.data.validationErrors});
+    }   
+  }
      this.setState({pendingApiCall:false})
   };
 
   render() {
-    const { pendingApiCall } = this.state;
+    const { pendingApiCall , errors } = this.state;
+    const { username,displayName} = errors ;
     return (
       <div className="container">
         <form>
           <h1 className="text-center">Sign Up</h1>
           <div className="form-group">
             <label>Username</label>
-            <input className="form-control" name="username" onChange={this.onChange} />
+            <input className={username ?  "form-control is-invalid" : "form-control"} name="username" onChange={this.onChange} />
+            <div className="invalid-feedback">    {username} </div>
           </div>
           <div className="form-group">
             <label>Display Name</label>
-            <input className="form-control" name="displayName" onChange={this.onChange} />
+            <input className={displayName ?  "form-control is-invalid" : "form-control"} name="displayName" onChange={this.onChange} />
+            <div className="invalid-feedback">    {displayName} </div>
           </div>
           <div className="form-group">
             <label>Password</label>
