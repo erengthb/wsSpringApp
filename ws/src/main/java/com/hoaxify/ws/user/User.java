92f8f0a821ff41a913c84536ceedcbc2449bed7a
Eren <erndmrhn@gmail.com>
2023-02-20 15:18:20 +0300

    remaining validations

--- ws/src/main/java/com/hoaxify/ws/user/User.java
+++ ws/src/main/java/com/hoaxify/ws/user/User.java
@@ -6,7 +6,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -19,11 +20,16 @@ public class User {
  private Long id;	
 	
  @NotBlank
+ @Size(min = 4 , max = 255)
  private String username;
  
  @NotBlank
+ @Size(min = 4 , max = 255)
  private String displayName;
  
+ @NotBlank
+ @Pattern(regexp="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$")
+ @Size(min = 8 , max = 30)
  private String password;
 	
 }
