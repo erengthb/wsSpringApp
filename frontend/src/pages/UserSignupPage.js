c9e81c1ac44df9155c4a66890179439970f2f8dd
Eren <erndmrhn@gmail.com>
2023-02-20 19:22:00 +0300

    Client side validation

--- frontend/src/pages/UserSignupPage.js
+++ frontend/src/pages/UserSignupPage.js
@@ -16,6 +16,16 @@ class UserSignupPage extends React.Component {
     const { name, value } = event.target;
     const errors = {...this.state.errors};
     errors[name] = undefined 
+    if(name ==='password' || name === 'passwordRepeat'){
+
+       if(name === 'password'  && value  !== this.state.passwordRepeat){
+          errors.passwordRepeat = 'Password mismatch';
+       } else if (name === 'passwordRepeat' && value !==this.state.password){
+          errors.passwordRepeat = 'Password mismatch';
+       } else {
+          errors.passwordRepeat = undefined;
+       }
+    }
     this.setState({
       [name]: value,
       errors
@@ -46,8 +56,8 @@ class UserSignupPage extends React.Component {
 
   render() {
     const { pendingApiCall , errors } = this.state;
-    const { username,displayName,password} = errors ;
-    return (
+    const { username,displayName,password,passwordRepeat} = errors ;
+    return ( 
       <div className="container">
         <form>
           <h1 className="text-center">Sign Up</h1>
@@ -55,14 +65,10 @@ class UserSignupPage extends React.Component {
              <Input name="username" label="Username" error={username} onChange={this.onChange}></Input>
              <Input name="displayName" label="Display Name" error={displayName} onChange={this.onChange}></Input>
              <Input name="password" label="Password" error={password} onChange={this.onChange} type="password" ></Input>
-           
-             <div className="form-group">
-               <label>Password Repeat</label>
-               <input className="form-control" name="passwordRepeat" type="password" onChange={this.onChange} />
-             </div>
+             <Input name="passwordRepeat" label="Password Repeat" error={passwordRepeat} onChange={this.onChange} type="password" ></Input>
 
              <div className="text-center">
-               <button className="btn btn-primary" onClick={this.onClickSignup} disabled={pendingApiCall}>
+               <button className="btn btn-primary" onClick={this.onClickSignup} disabled={pendingApiCall || passwordRepeat !== undefined}>
                {pendingApiCall && <span className="spinner-border spinner-border-sm"></span>  } Sign Up
                </button>
              </div>
