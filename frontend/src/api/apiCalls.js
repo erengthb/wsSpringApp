630566aff32c754d165e377a03b937f5c301b0e7
Eren <erndmrhn@gmail.com>
2023-02-21 16:30:54 +0300

    i18n in spring

--- frontend/src/api/apiCalls.js
+++ frontend/src/api/apiCalls.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 
 export const signup = (body) => {
-return   axios.post('/api/1.0/users', body);
+return   axios.post('/api/1.0/users', body ,{ headers : {'accept-language':'tr' }});
 
 }
