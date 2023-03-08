fc9089f24eab3e1a17933d5734f8c5d9721bd398
Eren <erndmrhn@gmail.com>
2023-03-08 17:19:07 +0300

    ErrorHandler-for-All-Error-Cases

--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -33,26 +33,4 @@ public class UserController {
 						
 	}
 	
-	// hata mesajlarını api errora dönüştürür
-	
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public ApiError handleValidationException(MethodArgumentNotValidException exception) {
-		
-		ApiError error = new ApiError(400,"Validation Error","/api/1.0/users");
-		
-		Map<String , String > validationErrors = new HashMap<>();
-		
-		for( FieldError fieldError  : exception.getBindingResult().getFieldErrors()) {
-			validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());		
-		}
-		
-		error.setValidationErrors(validationErrors);
-		
-		return error;
-		
-	}
-	
-	
-
 }
--- ws/src/main/java/error/ErrorHandler.java
+++ ws/src/main/java/error/ErrorHandler.java
@@ -1,12 +1,19 @@
 package error;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.error.ErrorAttributeOptions.Include;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.WebRequest;
+
+
 
 
 
@@ -18,14 +25,26 @@ public class ErrorHandler implements ErrorController{
 	
 	@RequestMapping("/error")
 	ApiError handleError(WebRequest webRequest) {
-		Map <String , Object> attributes = this.errorAttributes.getErrorAttributes(webRequest, null);
+		Map <String , Object> attributes = this.errorAttributes.getErrorAttributes(webRequest, ErrorAttributeOptions.of(Include.MESSAGE,Include.BINDING_ERRORS));
 		String message = (String)attributes.get("message");
 		String path    = (String)attributes.get("path");
 		int status       = (int)attributes.get("status");
+		ApiError error = new ApiError(status, message, path);
 		
-		return new ApiError(status, message, path);
+		if(attributes.containsKey("errors")) {
+			@SuppressWarnings("unchecked")
+			List<FieldError> fieldErrors = (List<FieldError>)attributes.get("errors");
+	        Map<String , String > validationErrors = new HashMap<>();
+			
+			for( FieldError fieldError  :fieldErrors) {
+				validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());	
+				
+			}
+			error.setValidationErrors(validationErrors); 
+		}
+				
+		return error;
 		
 	}
-	
 	
 }
