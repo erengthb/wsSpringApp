e4d96fe5b378c486b310f29e1e9a50f0721a1049
Eren <erndmrhn@gmail.com>
2023-03-16 14:24:21 +0300

    Authentication-success--response-3

--- ws/src/main/java/com/hoaxify/ws/shared/CurrentUser.java
+++ ws/src/main/java/com/hoaxify/ws/shared/CurrentUser.java
@@ -1,14 +1,15 @@
 package com.hoaxify.ws.shared;
 
-import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
 @Target({ElementType.PARAMETER}) //annotasyonu nerede kullanacağımızı belirttiğimiz yer
 @Retention(RUNTIME)
+@AuthenticationPrincipal
 public @interface CurrentUser {
 
 }
