c818bb12f7cc5fc0390f8b8a3a3b4da865ef5c97
Eren <erndmrhn@gmail.com>
2023-04-07 16:33:35 +0300

    Redux Initial Impl

--- frontend/src/components/TopBar.js
+++ frontend/src/components/TopBar.js
@@ -4,54 +4,70 @@ import { Link } from 'react-router-dom';
 import { withTranslation } from 'react-i18next';
 import { signup } from '../api/apiCalls';
 //import { Authentication } from '../shared/AuthenticationContext';
+import { connect } from 'react-redux';
 
 class TopBar extends Component {
-
-    //static contextType = Authentication;
-
+    // static contextType = Authentication;
+  
+    onClickLogout = () => {
+      const action = {
+        type: 'logout-success'
+      };
+      this.props.dispatch(action);
+    };
+  
     render() {
-       
-        const {t} = this.props;
-        const  onLogoutSuccess = () =>{}
-        const  isLoggedIn  = false;
-        const   username   = undefined ;      
-                    
-        let links = (
-            <ul className="navbar-nav ml-auto">
-                  <Link className='nav-link' to="/login">                 
-                         {t('Login')}                      
-                  </Link>
-                  <Link className='nav-link' to="/signup">                       
-                         {t('Sign Up')}                          
-                  </Link>
-            </ul>
+      const { t, username, isLoggedIn } = this.props;
+      let links = (
+        <ul className="navbar-nav ml-auto">
+          <li>
+            <Link className="nav-link" to="/login">
+              {t('Login')}
+            </Link>
+          </li>
+          <li>
+            <Link className="nav-link" to="/signup">
+              {t('Sign Up')}
+            </Link>
+          </li>
+        </ul>
+      );
+      if (isLoggedIn) {
+        links = (
+          <ul className="navbar-nav ml-auto">
+            <li>
+              <Link className="nav-link" to={`/user/${username}`}>
+                {username}
+              </Link>
+            </li>
+            <li className="nav-link" onClick={this.onClickLogout} style={{ cursor: 'pointer' }}>
+              {t('Logout')}
+            </li>
+          </ul>
         );
-
-        if(isLoggedIn){
-            links = (
-                <ul className="navbar-nav ml-auto">
-                    <li> 
-                        <Link className='nav-link' to={`/user/${username}`}> 
-                        {username}
-                        </Link>                       
-                    </li> 
-                    <li className='nav-link'  onClick={onLogoutSuccess}  style = {{cursor:'pointer'}}> {t('Logout')} </li> 
-                </ul>
-            );
-        }
-
-        return (
-            <div className="shadow-sm bg-light mb-2">               
-                <nav className="navbar navbar-light  container navbar-expand">
-                   <Link className="navbar-brand" to="/">
-                   <img src = {logo}  width = "60" alt ="Hoaxify Logo"></img>
-                    Hoaxify
-                    </Link>
-                    {links}
-                </nav>
-            </div>
-        );
+      }
+  
+      return (
+        <div className="shadow-sm bg-light mb-2">
+          <nav className="navbar navbar-light container navbar-expand">
+            <Link className="navbar-brand" to="/">
+              <img src={logo} width="60" alt="Hoaxify Logo" />
+              Hoaxify
+            </Link>
+            {links}
+          </nav>
+        </div>
+      );
     }
-}
-
-export default withTranslation()(TopBar);
+  }
+  
+  const TopBarWithTranslation = withTranslation()(TopBar);
+  
+  const mapStateToProps = store => {
+    return {
+      isLoggedIn: store.isLoggedIn,
+      username: store.username
+    };
+  };
+  
+  export default connect(mapStateToProps)(TopBarWithTranslation);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,16 +4,44 @@ import './index.css';
 import './bootstrap-override.scss';
 import reportWebVitals from './reportWebVitals';
 import './i18n';
-import AuthenticationContext from './shared/AuthenticationContext';
+// import AuthenticationContext from './shared/AuthenticationContext';
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore} from 'redux'
 
 import App from './container/App';
+
+
+const loggedInState = {
+  isLoggedIn: true,
+  username: 'user1',
+  displayName: 'display1',
+  image: null,
+  password: 'P4ssword'
+};
+
+const defaultState = {
+  isLoggedIn: false,
+  username: undefined,
+  displayName: undefined,
+  image: undefined,
+  password: undefined
+};
+
+const reducer = (state = { ...defaultState }, action) => {
+  if (action.type === 'logout-success') {
+    return defaultState;
+  }
+  return state;
+};
+
+const store = createStore(reducer, loggedInState);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <div>
-      
-         <App/>   
-
+      <Provider store ={store} >
+          <App/>   
+      </Provider>
 </div>
 );
 
