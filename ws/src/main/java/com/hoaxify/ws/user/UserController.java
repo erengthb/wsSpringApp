78b2be9f95d209d32dada09fc5862a5d47f70956
Eren <erndmrhn@gmail.com>
2023-07-19 13:47:18 +0300

    Returning Api Error to ValidationErrors

--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -1,11 +1,21 @@
 package com.hoaxify.ws.user;
 
-
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+
 import com.hoaxify.ws.shared.GenericResponse;
+
+import error.ApiError;
 import jakarta.validation.Valid;
 
 @RestController
@@ -16,11 +26,33 @@ public class UserController {
 
 	@PostMapping("/api/1.0/users")	
 	public GenericResponse createUser(@Valid @RequestBody User user ) {
-			
+		
 		userService.save(user);
 	
 		return new GenericResponse("user created");
-						
+				
+		
 	}
 	
+	// hata mesajlarını api errora dönüştürür
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ApiError handleValidationException(MethodArgumentNotValidException exception) {
+		
+		ApiError error = new ApiError(400,"Validation Error","/api/1.0/users");
+		
+		Map<String , String > validationErrors = new HashMap<>();
+		
+		for( FieldError fieldError  : exception.getBindingResult().getFieldErrors()) {
+			validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());		
+		}
+		
+		error.setValidationErrors(validationErrors);
+		
+		return error;
+		
+	}
+	
+
 }
