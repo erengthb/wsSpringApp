edb72ed6e9184a5f308f77d0750cd23f25fed37d
Eren <erndmrhn@gmail.com>
2023-07-26 19:29:58 +0300

    UserListItem

--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { getUsers } from '../api/apiCalls';
 import { withTranslation } from 'react-i18next';
+import UserListItem from './UserListItem';
 
 class UserList extends Component {
   state = {
@@ -23,9 +24,9 @@ class UserList extends Component {
         <h3 className="card-header text-center">{t('Users')}</h3>
         <div className="list-group-flush">
           {users.map(user => (
-            <div className="list-group-item list-group-item-action" key={user.username}>
-              {user.username}
-            </div>
+            <UserListItem  key={user.username} 
+            user ={user}>
+            </UserListItem>
           ))}
         </div>
       </div>
