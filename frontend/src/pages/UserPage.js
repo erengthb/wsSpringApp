3a320060bc228945a1085f6dd9de726298b15ea3
Eren <erndmrhn@gmail.com>
2023-09-15 17:59:08 +0300

    Load User Data in UserPage

--- frontend/src/pages/UserPage.js
+++ frontend/src/pages/UserPage.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProfileCard from '../components/ProfileCard';
+import { getUser } from '../api/apiCalls';
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
 
-const UserPage = (props) => {
+const UserPage = () => {
+    const [user , setUser] = useState();
+    const [username] = useParams();
+
+    useEffect(() => {
+        const loadUser = async() => {
+            try {
+                const response =   await getUser(username)
+                setUser(response.data)  
+            } catch (error){}
+        };
+        loadUser();
+    }, [username]);
+
     return (
         <div className="container"> 
             <ProfileCard >
