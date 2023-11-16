0cf616030bdd45327862910ce7f3ab1ac7c552a1
Eren <erndmrhn@gmail.com>
2023-11-16 15:52:59 +0300

    Updating Other Users Data

--- frontend/src/components/LanguageSelector.js
+++ frontend/src/components/LanguageSelector.js
@@ -3,30 +3,39 @@ import { useTranslation } from 'react-i18next';
 import { changeLanguage } from '../api/apiCalls';
 
 const LanguageSelector = (props) => {
+  const { i18n } = useTranslation();
 
-  const {i18n} = useTranslation();
+  const onChangeLanguage = (language) => {
+    i18n.changeLanguage(language);
+    changeLanguage(language);
+  };
 
-  const  onChangeLanguage = language => {
-        i18n.changeLanguage(language);
-        changeLanguage(language);
-    };
-
-    return (
-        <div className="container">
-            <img
-              src="https://www.countryflagicons.com/FLAT/24/TR.png"
-              alt="Turkish Flag"
-              onClick={() => onChangeLanguage('tr')}
-              style={{ cursor: 'pointer' }}
-            ></img>
-            <img
-              src="https://www.countryflagicons.com/FLAT/24/GB.png"
-              alt="USA Flag"
-              onClick={() => onChangeLanguage('en')}
-              style={{ cursor: 'pointer' }}
-            ></img>
-          </div>
-    );
+  return (
+    <div className="container">
+      <div
+        className="flag-container"
+        onClick={() => onChangeLanguage('tr')}
+        style={{ display: 'inline-block', marginRight: '10px' }}
+      >
+        <img
+          src="https://flagcdn.com/w20/tr.png"
+          srcSet="https://flagcdn.com/w40/tr.png 2x"
+          width="20"
+          alt="Turkey"
+          style={{ cursor: 'pointer' }}
+        ></img>
+      </div>
+      <div className="flag-container" onClick={() => onChangeLanguage('en')} style={{ display: 'inline-block', marginRight: '10px' }}>
+        <img
+          src="https://flagcdn.com/w20/us.png"
+          srcSet="https://flagcdn.com/w40/us.png 2x"
+          width="20"
+          alt="United States"
+          style={{ cursor: 'pointer' }}
+        ></img>
+      </div>
+    </div>
+  );
 };
 
 export default LanguageSelector;
