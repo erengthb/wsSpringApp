909a87b6466937095c0b9b8debc0221a5db29159
Eren <erndmrhnn@gmail.com>
2024-05-20 16:43:49 +0300

    Serving static file backend

--- ws/src/main/java/com/hoaxify/ws/configuration/WebConfiguration.java
+++ ws/src/main/java/com/hoaxify/ws/configuration/WebConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer{
-
+	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/images/**")
