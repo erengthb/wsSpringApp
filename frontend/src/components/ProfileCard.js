0e14a88e623118dba29b05cd8697d2efbc2a8561
Eren <erndmrhnn@gmail.com>
2024-07-16 23:14:43 +0300

    display name validatons

--- frontend/src/components/ProfileCard.js
+++ frontend/src/components/ProfileCard.js
@@ -17,6 +17,7 @@ const ProfileCard = props => {
   const [user, setUser] = useState({});
   const [editable, setEditable] = useState(false);
   const [newImage, setNewImage] = useState();
+  const[validationErrors, setValidationErrors] = useState({});
 
   useEffect(() => {
     setUser(props.user);
@@ -26,6 +27,13 @@ const ProfileCard = props => {
   useEffect(() => {
     setEditable(pathUsername === loggedInUsername);
   }, [pathUsername, loggedInUsername]);
+
+  useEffect(() => {
+    setValidationErrors(previousValidationErrors => ({
+        ...previousValidationErrors,
+        displayName:undefined  
+    }));
+  }, [updatedDisplayName]);
 
   const { username, displayName, image } = user;
   const { t } = useTranslation();
@@ -52,7 +60,9 @@ const ProfileCard = props => {
       const response = await updateUser(username, body);
       setInEditMode(false);
       setUser(response.data);
-    } catch (error) {}
+    } catch (error) {
+      setValidationErrors(error.response.data.validationErrors)
+    }
   };
 
   const onChangeFile = event => {
@@ -69,6 +79,7 @@ const ProfileCard = props => {
 
   const pendingApiCall = useApiProgress('put', '/api/1.0/users/' + username);
 
+  const {displayName : displayNameError } = validationErrors;
   return (
     <div className="card text-center">
       <div className="card-header">
@@ -103,6 +114,7 @@ const ProfileCard = props => {
               onChange={event => {
                 setUpdatedDisplayName(event.target.value);
               }}
+              error = {displayNameError}
             />
             <input type="file" onChange={onChangeFile} />
             <div>
