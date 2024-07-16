28da6bcb5aa3a117725ca0dbf9cf0227501f550b
Eren <erndmrhnn@gmail.com>
2024-07-16 23:14:31 +0300

    Display Name validatons

--- ws/src/main/java/com/hoaxify/ws/user/vm/UserUpdateVM.java
+++ ws/src/main/java/com/hoaxify/ws/user/vm/UserUpdateVM.java
@@ -1,10 +1,14 @@
 package com.hoaxify.ws.user.vm;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UserUpdateVM {
 
+	@NotBlank
+	@Size(min = 4 , max = 255)
 	private String displayName;
 
 	private String image;
