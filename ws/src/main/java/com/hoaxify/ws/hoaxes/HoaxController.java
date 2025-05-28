7bb8aa27159cf99d055d1e706c24bbf2b1e2ffe1
Eren DEMİRHAN <GTUSR0728@gtech.com.tr>
2025-05-28 17:15:18 +0300

    Hoax Sorting

--- ws/src/main/java/com/hoaxify/ws/hoaxes/HoaxController.java
+++ ws/src/main/java/com/hoaxify/ws/hoaxes/HoaxController.java
@@ -3,6 +3,8 @@ package com.hoaxify.ws.hoaxes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,7 +29,7 @@ public class HoaxController {
 	}
 
 	@GetMapping("/hoaxes")
-	public Page<Hoax> getHoaxes(Pageable page) {
+	public Page<Hoax> getHoaxes(@PageableDefault(sort = "id" , direction = Direction.DESC) Pageable page) {
 		return hoaxService.getHoaxes(page);
 	}
 }
--- ws/src/main/java/com/hoaxify/ws/user/User.java
+++ ws/src/main/java/com/hoaxify/ws/user/User.java
@@ -49,25 +49,23 @@ public class User implements UserDetails {
 
 	@Override
 	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
+		
 		return true;
 	}
 
 	@Override
 	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
+	
 		return true;
 	}
 
 	@Override
 	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isEnabled() {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
