1af0c453ace96f2540cd42e1626b6048fa6c2dbd
erngthb <erndmrhnn@gmail.com>
2025-06-02 11:12:51 +0300

    HoaxView  - New Fields

--- frontend/src/components/HoaxView.js
+++ frontend/src/components/HoaxView.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import ProfileImageWithDefault from './ProfileImageWithDefault';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 const HoaxView = (props) => {
   const { hoax } = props;
+  const{user,content} = hoax;
+  const{username,displayName,image} = user;
+
   return (
     <div>
-      <div className='card p-1'>{hoax.content}</div>
+      <div className='card p-1'>
+        <div className="d-flex">
+        <ProfileImageWithDefault image={image} width="32" height="32" className="rounded-circle m-1" />
+        <div className ="flex-fill m-auto pl-2">
+            <Link to={`/user/${username}`} className="text-decoration-none text-dark">
+             <h6>{displayName}@{username}</h6>
+            </Link>
+        </div>
+        
+        
+        </div>
+          <div className="pl-5">
+            {content}
+          </div>
+      
+      </div>
     </div>
   )
 }
