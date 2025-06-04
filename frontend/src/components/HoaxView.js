9ecb310c595afa5e67dc35abd6c7f208f2ecb6aa
erngthb <erndmrhnn@gmail.com>
2025-06-04 16:24:11 +0300

    Prop Types Validaton

--- frontend/src/components/HoaxView.js
+++ frontend/src/components/HoaxView.js
@@ -1,32 +1,41 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 import ProfileImageWithDefault from './ProfileImageWithDefault';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 const HoaxView = (props) => {
   const { hoax } = props;
-  const{user,content} = hoax;
-  const{username,displayName,image} = user;
+  const { user, content } = hoax;
+  const { username, displayName, image } = user;
 
   return (
     <div>
       <div className='card p-1'>
         <div className="d-flex">
-        <ProfileImageWithDefault image={image} width="32" height="32" className="rounded-circle m-1" />
-        <div className ="flex-fill m-auto pl-2">
+          <ProfileImageWithDefault image={image} width="32" height="32" className="rounded-circle m-1" />
+          <div className="flex-fill m-auto pl-2">
             <Link to={`/user/${username}`} className="text-decoration-none text-dark">
-             <h6>{displayName}@{username}</h6>
+              <h6>{displayName}@{username}</h6>
             </Link>
-        </div>
-        
-        
-        </div>
-          <div className="pl-5">
-            {content}
           </div>
-      
+        </div>
+        <div className="pl-5">
+          {content}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HoaxView
+HoaxView.propTypes = {
+  hoax: PropTypes.shape({
+    content: PropTypes.string.isRequired,
+    user: PropTypes.shape({
+      username: PropTypes.string.isRequired,
+      displayName: PropTypes.string.isRequired,
+      image: PropTypes.string,
+    }).isRequired,
+  }).isRequired,
+};
+
+export default HoaxView;
