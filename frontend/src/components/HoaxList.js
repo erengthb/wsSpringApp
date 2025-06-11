0ab8006d78b9568b97c7dd181233fa1f0b484cea
erngthb <erndmrhnn@gmail.com>
2025-06-11 12:51:13 +0300

    Hoaxlist in User Page

--- frontend/src/components/HoaxList.js
+++ frontend/src/components/HoaxList.js
@@ -2,48 +2,54 @@ import React, { useState, useEffect } from 'react';
 import { getHoaxes } from '../api/apiCalls';
 import { useTranslation } from 'react-i18next';
 import HoaxView from './HoaxView';
-import Spinner from './Spinner';
 import { useApiProgress } from '../shared/ApiProgress';
+import Spinner from './Spinner';
+import { useParams } from 'react-router-dom';
 
 const HoaxList = () => {
-
-  const [hoaxPage, setHoaxPage] = useState({ content: [] , last: true, number: 0});
+  const [hoaxPage, setHoaxPage] = useState({ content: [], last: true, number: 0 });
   const { t } = useTranslation();
- 
- 
-  const pendingApiCall = useApiProgress('get', '/api/1.0/hoaxes');
+  const { username } = useParams();
+
+  const path = username ? `/api/1.0/users/${username}/hoaxes?page=` : '/api/1.0/hoaxes?page=';
+  const pendingApiCall = useApiProgress('get', path);
 
   useEffect(() => {
     loadHoaxes();
   }, []);
-      
+
   const loadHoaxes = async page => {
     try {
-      const response = await getHoaxes(page);
+      const response = await getHoaxes(username, page);
       setHoaxPage(previousHoaxPage => ({
         ...response.data,
         content: [...previousHoaxPage.content, ...response.data.content]
-      })) ;
+      }));
     } catch (error) {}
   };
 
+  const { content, last, number } = hoaxPage;
 
-  const {content, last, number} = hoaxPage
-
-  if (hoaxPage.content.length === 0) {
+  if (content.length === 0) {
     return <div className="alert alert-secondary text-center">{pendingApiCall ? <Spinner /> : t('There are no hoaxes')}</div>;
   }
 
   return (
-  <div>
-    {content.map(hoax => {
-      return <HoaxView key={hoax.id} hoax={hoax} />
-    })}
-    {!last && <div className="alert alert-secondary text-center" 
-       style={{cursor: pendingApiCall ? 'not-allowed' : 'pointer'}}
-       onClick={pendingApiCall ? () => {} : () => loadHoaxes(number + 1)}>
-      {pendingApiCall ? <Spinner /> : t('Load old hoaxes')}</div>}
-  </div>
-);
+    <div>
+      {content.map(hoax => {
+        return <HoaxView key={hoax.id} hoax={hoax} />;
+      })}
+      {!last && (
+        <div
+          className="alert alert-secondary text-center"
+          style={{ cursor: pendingApiCall ? 'not-allowed' : 'pointer' }}
+          onClick={pendingApiCall ? () => {} : () => loadHoaxes(number + 1)}
+        >
+          {pendingApiCall ? <Spinner /> : t('Load old hoaxes')}
+        </div>
+      )}
+    </div>
+  );
 };
+
 export default HoaxList;
--- frontend/src/components/HoaxView.js
+++ frontend/src/components/HoaxView.js
@@ -1,40 +1,34 @@
-import React from 'react'
+import React from 'react';
 import ProfileImageWithDefault from './ProfileImageWithDefault';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
-import {format} from 'timeago.js';
+import { Link } from 'react-router-dom';
+import { format } from 'timeago.js';
 import { useTranslation } from 'react-i18next';
 
-const HoaxView = (props) => {
+const HoaxView = props => {
   const { hoax } = props;
-  const{user , content , timestamp} = hoax;
-  const{username , displayName , image} = user;
+  const { user, content, timestamp } = hoax;
+  const { username, displayName, image } = user;
 
+  const { i18n } = useTranslation();
 
-  const {i18n} = useTranslation();
-  const formatted  = format(timestamp ,i18n.language);
-
+  const formatted = format(timestamp, i18n.language);
   return (
-    <div>
-      <div className='card p-1'>
-        <div className="d-flex">
+    <div className="card p-1">
+      <div className="d-flex">
         <ProfileImageWithDefault image={image} width="32" height="32" className="rounded-circle m-1" />
-        <div className ="flex-fill m-auto pl-2">
-            <Link to={`/user/${username}`} className="text-decoration-none text-dark">
-             <h6 className ="d-inline"> {displayName}@{username}</h6>                                             
-             <span>  --  </span>
-             <span>{formatted}</span>
-            </Link>
+        <div className="flex-fill m-auto pl-2">
+          <Link to={`/user/${username}`} className="text-dark">
+            <h6 className="d-inline">
+              {displayName}@{username}
+            </h6>
+            <span> - </span>
+            <span>{formatted}</span>
+          </Link>
         </div>
-        
-        
-        </div>
-          <div className="pl-5">
-            {content}
-          </div>
-      
       </div>
+      <div className="pl-5">{content}</div>
     </div>
-  )
-}
+  );
+};
 
-export default HoaxView
+export default HoaxView;
