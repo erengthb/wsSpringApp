1fc9c68a4ad065e71137d262c2908327254905f4
erngthb <erndmrhnn@gmail.com>
2025-06-20 15:19:03 +0300

    Scheduled Log Remove Service

--- ws/src/main/java/com/hoaxify/ws/scheduled/LogCleanupService.java
+++ ws/src/main/java/com/hoaxify/ws/scheduled/LogCleanupService.java
@@ -3,6 +3,7 @@ package com.hoaxify.ws.scheduled;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.hoaxify.ws.log.LogRecordRepository;
 
@@ -14,10 +15,12 @@ public class LogCleanupService {
     @Autowired
     private LogRecordRepository logRecordRepository;
 
-  
-    @Scheduled(cron = "${app.log-cleanup.cron}") // milisaniye cinsinden: 1 gün
+    @Transactional
+    @Scheduled(cron = "${app.log-cleanup.cron}", zone = "Europe/Istanbul")
     public void cleanupOldLogs() {
-        LocalDateTime threshold = LocalDateTime.now().minusHours(24);
+        LocalDateTime threshold = LocalDateTime.now().minusMinutes(1);
+        System.out.println("Log cleanup started at " + LocalDateTime.now());
         logRecordRepository.deleteByTimestampBefore(threshold);
+        System.out.println("Log cleanup finished at " + LocalDateTime.now());
     }
 }
