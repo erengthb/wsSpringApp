12c4a090093437312bc21908dee2ad7033d91520
erngthb <erndmrhnn@gmail.com>
2025-06-24 20:20:01 +0300

    uat cors settings

--- ws/src/main/java/com/hoaxify/ws/configuration/WebConfig.java
+++ ws/src/main/java/com/hoaxify/ws/configuration/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig {
                         .allowedOrigins(
                             "http://localhost:3000",
                             "https://ws-spring-app.vercel.app", // Production
-                            "https://ws-spring-pknymj774-erengthbs-projects.vercel.app")   // UAT
+                            "https://ws-spring-app-git-uat-erengthbs-projects.vercel.app")   // UAT
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
