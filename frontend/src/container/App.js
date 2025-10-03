5ee62caf11ceb3bff874f31f5b9afd4a0c9739a1
erngthb <erndmrhnn@gmail.com>
2025-10-03 18:11:45 +0300

    passive language selector

--- frontend/src/container/App.js
+++ frontend/src/container/App.js
@@ -41,7 +41,7 @@ const App = () => {
           </Switch>
         </div>
 
-        <LanguageSelector />
+        {/* <LanguageSelector />*/}
         <WhatsAppButton />
         <Footer />
       </Router>
